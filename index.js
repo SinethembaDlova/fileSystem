500ab3389f6a48f5c6e9a9d5813a68f9e278dae3
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 01:59:04 +0200

    read the text that I wrote in the file.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const { error } = require('console');
 const fs = require('fs')
 
-fs.writeFile('./src/assets/file.txt', 'Hello World, my name is Sinethemba Dlova', (err) =>{
+fs.writeFile('./src/assets/file.txt', 'Hello World, my name is Sinethemba Dlova', (err) => {
     if (err) throw err;
     console.log('Succefully wrote in a file.');
+
+    fs.readFile('./src/assets/file.txt', (err, text) => {
+        if (err) throw err;
+        console.log('text: ', text);
+    })
 });
