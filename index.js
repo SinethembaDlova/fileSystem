4a68ffa59c50319365cec76428a7ba8d02a70365
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 01:55:02 +0200

    used the writeFile function to write in a file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('HELLO WORLD');
+const fs = require('fs')
+
+fs.writeFile('./src/assets/file.txt', 'Hello World, my name is Sinethemba Dlova', (err) =>{
+    if (err) throw err;
+    console.log('Succefully wrote in a file.');
+});
