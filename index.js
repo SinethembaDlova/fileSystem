11b20a02c88894ba81e918e3434a7fc887e57df5
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 03:50:27 +0200

    put the function inside one another's call back

--- index.js
+++ index.js
@@ -9,19 +9,18 @@ fs.writeFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'Hello World, my 
     // Added more text with fs appendFIle .
     fs.appendFile(path.join(__dirname, '/src/assets/', 'file.txt'), '\n I love programming', (err) => {
         if (err) throw err;
-        console.log('Succefully wrote in a file.');
+        console.log('Succefully appended in a file.');
 
-    })
-
-    // read the file created with fs readFile and played with the data.
-    fs.readFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'utf-8', (err, text) => {
-        if (err) throw err;
-        console.log('text: ', text);
-        const list = text.split(' ');
-        console.log('# of words: ', list.length)
-        list.map((word, index) => {
+        // read the file created with fs readFile and played with the data.
+        fs.readFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'utf-8', (err, text) => {
+            if (err) throw err;
+            console.log('text: ', text);
+            const list = text.split(' ');
+            console.log('# of words: ', list.length)
+            list.map((word, index) => {
             console.log(index + ": " + word);
         });
+    })
     })
 })
 
