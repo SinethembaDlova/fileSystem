86cd3c5547e55ef5f3e1f091bc4e2517929e3adf
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 05:12:55 +0200

    used fs existsSync function to remove a folder if it does exist

--- dir.js
+++ dir.js
@@ -4,6 +4,13 @@ const path = require('path');
 if (!fs.existsSync(path.join(__dirname, '/src/constants/'))){
     fs.mkdir(path.join(__dirname, '/src/constants/'), (err) => {
         if (err) throw err
-        console.log('Directory successfully createdno')
+        console.log('Directory successfully created')
+    })
+}
+
+if (fs.existsSync(path.join(__dirname, '/src/constants/'))){
+    fs.rmdir(path.join(__dirname, '/src/constants/'), (err) => {
+        if (err) throw err
+        console.log('Directory successfully removed')
     })
 }
