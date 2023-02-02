126fe0f1685e67373b6dfb11b4d6a77d966073f2
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 02:01:01 +0200

    the file was returning binary so I specified with utf-8 that we want the string

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ fs.writeFile('./src/assets/file.txt', 'Hello World, my name is Sinethemba Dlova'
     if (err) throw err;
     console.log('Succefully wrote in a file.');
 
-    fs.readFile('./src/assets/file.txt', (err, text) => {
+    fs.readFile('./src/assets/file.txt', 'utf-8', (err, text) => {
         if (err) throw err;
         console.log('text: ', text);
     })
