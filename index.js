171ae797a012c675d12706497280c01e223e0928
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 03:42:18 +0200

    used the appendFile method to add more text in file.txt

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ fs.writeFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'Hello World, my 
     if (err) throw err;
     console.log('Succefully wrote in a file.');
 
+    // Added more text with fs appendFIle .
+    fs.appendFile(path.join(__dirname, '/src/assets/', 'file.txt'), '\n I love programming', (err) => {
+        if (err) throw err;
+        console.log('Succefully wrote in a file.');
+
+    })
+
     // read the file created with fs readFile and played with the data.
     fs.readFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'utf-8', (err, text) => {
         if (err) throw err;
@@ -18,21 +25,6 @@ fs.writeFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'Hello World, my 
     })
 })
 
-// Created a new file with fs appendFIle and wrote on it.
-fs.appendFile(path.join(__dirname, '/src/assets/', 'file1.txt'), 'Hello World, I love programming', (err) => {
-    if (err) throw err;
-    console.log('Succefully wrote in a file.');
-
-    fs.readFile(path.join(__dirname, '/src/assets/', 'file1.txt'), 'utf-8', (err, text) => {
-        if (err) throw err;
-        console.log('text: ', text);
-        const list = text.split(' ');
-        console.log('# of words: ', list.length)
-        list.map((word, index) => {
-            console.log(index + ": " + word);
-        });
-    })
-})
 
 process.on('uncaughtException', err => {
     console.error('There was an uncaught error: ', err)
