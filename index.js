41621ec7a6f5e21c3516512789f3480b48ef5370
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 02:31:02 +0200

    used the path library so that we don't have path issues in different machines.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-fs.writeFile('./src/assets/file.txt', 'Hello World, my name is Sinethemba Dlova', (err) => {
+fs.writeFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'Hello World, my name is Sinethemba Dlova', (err) => {
     if (err) throw err;
     console.log('Succefully wrote in a file.');
 
-    fs.readFile('./src/assets/file.txt', 'utf-8', (err, text) => {
+    fs.readFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'utf-8', (err, text) => {
         if (err) throw err;
         console.log('text: ', text);
         const list = text.split(' ');
