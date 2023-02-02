0c6c1f6412cde8f589d99833eb300fb45938ff2b
Sinethemba <sinethemba@wealthmigrate.com>
2023-02-02 03:35:52 +0200

    created a new file and wrote on with the append file

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
+// Create a file with fs writeFile function and wrote on it.
 fs.writeFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'Hello World, my name is Sinethemba Dlova', (err) => {
     if (err) throw err;
     console.log('Succefully wrote in a file.');
 
+    // read the file created with fs readFile and played with the data.
     fs.readFile(path.join(__dirname, '/src/assets/', 'file.txt'), 'utf-8', (err, text) => {
+        if (err) throw err;
+        console.log('text: ', text);
+        const list = text.split(' ');
+        console.log('# of words: ', list.length)
+        list.map((word, index) => {
+            console.log(index + ": " + word);
+        });
+    })
+})
+
+// Created a new file with fs appendFIle and wrote on it.
+fs.appendFile(path.join(__dirname, '/src/assets/', 'file1.txt'), 'Hello World, I love programming', (err) => {
+    if (err) throw err;
+    console.log('Succefully wrote in a file.');
+
+    fs.readFile(path.join(__dirname, '/src/assets/', 'file1.txt'), 'utf-8', (err, text) => {
         if (err) throw err;
         console.log('text: ', text);
         const list = text.split(' ');
